815169253f87a864dcb814786cdde58c748bb47f
oscartavarez <tavarez.oscar>
2018-12-05 08:41:57 -0400

    setting defaults

--- src/Config.php
+++ src/Config.php
@@ -1,13 +1,13 @@
 <?php
 	class Config{
 		public $dbdriver = 'mysql';
-		public $dbname = 'demodev_schoolworx';
-		public $dbprefix = 'jos_';
+		public $dbname = 'test';
+		public $dbprefix = 'test_';
 		public $dbuser = 'root';
 	   	public $dbpass = '';
-		public $dbhost = 'msw1-db';
+		public $dbhost = 'localhost';
 		public $dboptions = [PDO::MYSQL_ATTR_INIT_COMMAND => 'SET sql_mode=""'];
-		public $path = '/tools/quick_tables/src/';
+		public $path = './quick_tables/src/';
 		public $secret = 'dB48TjkTJFuYXzHptPw2c/01XOWNRnfl33+Xy/4gpFQ=';
 	}
 
